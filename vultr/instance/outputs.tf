22fac0aac0eb3b2ac336c2b07d819bec5c44e29b
Marcos Vanetta <marcos.vanetta@nytimes.com>
2022-06-13 11:24:17 -0500

    Remove Vultr

--- vultr/instance/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ip" {
-  value = vultr_instance.eva.main_ip
-}
-
-output "fwg_id" {
-  value = vultr_firewall_group.fwg.id
-}
--- vultr/instance/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "api_key" {
-  description = "Your Vultr API personal token. (required)"
-}
